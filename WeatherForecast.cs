94767a2d91475b671382130bd8d9571e099d0d34
Leonid <qwertyleo2121@gmail.com>
2021-10-31 15:41:10 +0300

    init project commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Team_Let1m_carShop
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
