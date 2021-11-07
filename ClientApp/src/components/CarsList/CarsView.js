9d8059acf0c9924f1920ac269368198741cf0252
mviiia <vinmarie@yandex.ru>
2021-11-07 21:25:36 +0300

    cards added

--- ClientApp/src/components/CarsList/CarsView.js
+++ ClientApp/src/components/CarsList/CarsView.js
@@ -1,5 +1,5 @@
-﻿import { Button } from 'reactstrap';
-import React, { Component } from 'react';
+﻿import React, { Component } from 'react';
+import { CardGroup, Card, CardImg, CardBody, CardTitle, CardSubtitle,CardText, Button} from 'reactstrap';
 
 export class CarsView extends Component {
 
@@ -8,7 +8,83 @@ export class CarsView extends Component {
         return (
             <div>
             <h2>CARS</h2>
-            <Button class="Btn-danger">hehe</Button>
+            <CardGroup>
+              <Card>
+                <CardImg
+                  alt="Card image cap"
+                  src="https://picsum.photos/318/180"
+                  top
+                  width="100%"
+                />
+                <CardBody>
+                  <CardTitle tag="h5">
+                    Card title
+                  </CardTitle>
+                  <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                  >
+                    Card subtitle
+                  </CardSubtitle>
+                  <CardText>
+                    This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                  </CardText>
+                  <Button>
+                    Button
+                  </Button>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardImg
+                  alt="Card image cap"
+                  src="https://picsum.photos/318/180"
+                  top
+                  width="100%"
+                />
+                <CardBody>
+                  <CardTitle tag="h5">
+                    Card title
+                  </CardTitle>
+                  <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                  >
+                    Card subtitle
+                  </CardSubtitle>
+                  <CardText>
+                    This card has supporting text below as a natural lead-in to additional content.
+                  </CardText>
+                  <Button>
+                    Button
+                  </Button>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardImg
+                  alt="Card image cap"
+                  src="https://picsum.photos/318/180"
+                  top
+                  width="100%"
+                />
+                <CardBody>
+                  <CardTitle tag="h5">
+                    Card title
+                  </CardTitle>
+                  <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                  >
+                    Card subtitle
+                  </CardSubtitle>
+                  <CardText>
+                    This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
+                  </CardText>
+                  <Button>
+                    Button
+                  </Button>
+                </CardBody>
+              </Card>
+            </CardGroup>
             </div>
             );
 
